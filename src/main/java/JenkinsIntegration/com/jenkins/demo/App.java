d98c439f48f4bd8813bdfcb866c18fd4bf7768bf
Dhakate, Swapnil <swapnildhakate54@gmail.com>
2021-06-17 21:00:43 +0530

    adding Hello world file

---
+++ src/main/java/JenkinsIntegration/com/jenkins/demo/App.java
@@ -0,0 +1,13 @@
+package JenkinsIntegration.com.jenkins.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
